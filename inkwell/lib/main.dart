1853ad9557972289a4c193e1b0598bfde128e238
stevenleonard271 <stevenleonard271@gmail.com>
2021-08-29 23:09:14 +0700

    My first upload on second part

---
+++ inkwell/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+//Membuat button sendiri dengan gradasi menggunakan InkWell
+
+//Ink well membuat button ketika diklik dapat memberikan respon berupa splash
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan Membuat Button'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {},
+                shape: StadiumBorder(),
+                child: Text('Raised Button'),
+              ),
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.pink],
+                        begin: Alignment.center,
+                        end: Alignment.bottomCenter,
+                      )),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      splashColor: Colors.amber,
+                      onTap: () {},
+                      child: Center(
+                          child: Text(
+                        'My Button',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w600),
+                      )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
